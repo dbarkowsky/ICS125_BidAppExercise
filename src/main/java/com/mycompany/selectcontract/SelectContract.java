2573af59a319fd19f3b7200085efa6bd86d83bcf
Dylan Barkowsky <dylanbarkowsky@gmail.com>
2022-02-28 17:16:41 -0800

    Initial commit. MVC classes and main class started.

---
+++ src/main/java/com/mycompany/selectcontract/SelectContract.java
@@ -0,0 +1,20 @@
+
+package com.mycompany.selectcontract;
+
+/**
+ *
+ * @author Dylan Barkowsky
+ */
+public class SelectContract {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ContractView theView = new ContractView();
+        ContractModel theModel = new ContractModel();
+        ContractController theController = new ContractController(theView, theModel);
+        theView.setVisible(true);
+    }
+    
+}
